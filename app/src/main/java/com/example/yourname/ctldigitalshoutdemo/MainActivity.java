4e3ce3a15611a3c71ec2202bd80b114c62855fa9
BlueBug <iopzxz@gmail.com>
2018-09-25 11:37:54 -0400

    Added demo code; copy-pasted

--- app/src/main/java/com/example/yourname/ctldigitalshoutdemo/MainActivity.java
+++ app/src/main/java/com/example/yourname/ctldigitalshoutdemo/MainActivity.java
@@ -1,18 +1,76 @@
+/*
+September 24, 2018
+https://code.tutsplus.com/tutorials/google-play-services-using-the-nearby-connections-api--cms-24534
+
+ */
 package com.example.yourname.ctldigitalshoutdemo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.android.gms.location.*;
 
 public class MainActivity extends AppCompatActivity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR)
+				!= PackageManager.PERMISSION_GRANTED) {
+			// Permission is not granted
+			// Should we show an explanation?
+			if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+					Manifest.permission.READ_CONTACTS)) {
+				// Show an explanation to the user *asynchronously* -- don't block
+				// this thread waiting for the user's response! After the user
+				// sees the explanation, try again to request the permission.
+			} else {
+				// No explanation needed; request the permission
+				ActivityCompat.requestPermissions(this,
+						new String[]{Manifest.permission.READ_CONTACTS},
+						0); //it is supposed to be MY_PERMISSIONS_REQUEST_READ_CONTACTS not 0
+
+				// MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+				// app-defined int constant. The callback method gets the
+				// result of the request.
+			}
+		} else {
+			// Permission has already been granted
+		}
+
+		setContentView(R.layout.activity_main);
+		FusedLocationProviderClient client =
+				LocationServices.getFusedLocationProviderClient(this);
+
+	}
+	/*
+	from https://developer.android.com/training/permissions/requesting#java
+	When the user responds to your app's permission request, the system invokes your app's onRequestPermissionsResult() method, passing it the user response. Your app has to override that method to find out whether the permission was granted. The callback is passed the same request code you passed to requestPermissions(). For example, if an app requests READ_CONTACTS access it might have the following callback method:
+	 */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            // I dont know what MY_PERMISSIONS_REQUEST_READ_CONTACTS is supposed to be.
+            default:
+            case 0: { //IT is supposed to be MY_PERMISSIONS_REQUEST_READ_CONTACTS not 0 but since I don't know what MY_PERMISSIONS_REQUEST_READ_CONTACTS is, I am just leaving 0 here
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
 
-        setContentView(R.layout.activity_main);
-        FusedLocationProviderClient client =
-                LocationServices.getFusedLocationProviderClient(this);
-
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 }
